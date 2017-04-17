ff3bb4b7f3bcaee9d9716fc4473fbc53d544f020
Kamilo Vasquez <me@killoblan.co>
2017-04-17 01:23:49 -0500

    Update views for complete crud

--- core/scrumboard/views.py
+++ core/scrumboard/views.py
@@ -23,13 +23,26 @@ class IndexView(View):
         return render(request, self.template_name, context)
 
 
-class RequestsListView(ListView):
+class CreateRequestView(CreateView):
+    template_name = 'scrumboard/pages/form.html'
+    form_class = RequirementForm
+    success_url = reverse_lazy('core:scrumboard:read')
+
+
+class ReadRequestView(ListView):
     model = Requirements
     context_object_name = 'requirements'
     template_name = 'scrumboard/pages/list.html'
 
 
-class CreateRequestView(CreateView):
+class UpdateRequestView(UpdateView):
+    model = Requirements
     template_name = 'scrumboard/pages/form.html'
     form_class = RequirementForm
-    success_url = reverse_lazy('core:scrumboard:list')
+    success_url = reverse_lazy('core:scrumboard:read')
+
+
+class DeleteRequestView(DeleteView):
+    model = Requirements
+    template_name = 'scrumboard/pages/form_delete.html'
+    success_url = reverse_lazy('core:scrumboard:read')
