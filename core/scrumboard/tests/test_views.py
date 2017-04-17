df967aacc85ca209f219c43a07411490a03a9d0d
Kamilo Vasquez <me@killoblan.co>
2017-04-17 01:27:03 -0500

    Some tests

--- core/scrumboard/tests/test_views.py
+++ core/scrumboard/tests/test_views.py
@@ -1,7 +1,7 @@
 from django.db.models import QuerySet
 from django.test import TestCase, RequestFactory
 
-from core.scrumboard.views import IndexView, RequestsListView
+from core.scrumboard.views import *
 from core.scrumboard.models import Requirements
 
 
@@ -14,7 +14,7 @@ class IndexViewTestCase(TestCase):
         super().setUpClass()
 
 
-class RequestsListViewTestCase(TestCase):
+class ReadRequestViewTestCase(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
         self.url = '/admin/scrumboard/list'
@@ -26,7 +26,7 @@ class RequestsListViewTestCase(TestCase):
 
     def test_no_items(self):
         request = self.factory.get(self.url)
-        response = RequestsListView.as_view()(request)
+        response = ReadRequestView.as_view()(request)
 
         self.assertEqual(response.status_code, 200)
         # self.assertEqual(response.context_data['solo'].artist, 'Rich')
@@ -36,7 +36,23 @@ class RequestsListViewTestCase(TestCase):
     def test_items(self):
         Requirements.objects.create(detail="Test Detail", type="Feature", priority="5", deadline="2017-01-01")
         request = self.factory.get(self.url)
-        response = RequestsListView.as_view()(request)
+        response = ReadRequestView.as_view()(request)
+
+        self.assertEqual(response.status_code, 200)
+        with self.assertTemplateUsed(self.template):
+            response.render()
+
+
+class UpdateRequestViewTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory
+        self.url = 'admin/scrumboard/update/1/'
+        self.template = 'scrumboard/pages/form.html'
+
+    def test_update(self):
+        Requirements.objects.create(detail="Test Detail", type="Feature", priority="5", deadline="2017-01-01")
+        request = self.factory.get(self.url)
+        response = UpdateRequestView.as_view()(request)
 
         self.assertEqual(response.status_code, 200)
         with self.assertTemplateUsed(self.template):
