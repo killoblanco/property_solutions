614c53b0284ebbc63fb2bd03cd2b5b77f3f966ab
Kamilo Vasquez <me@killoblan.co>
2017-04-17 01:22:57 -0500

    Added urls for complete crud and api urls

--- core/scrumboard/urls.py
+++ core/scrumboard/urls.py
@@ -1,8 +1,11 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from core.scrumboard.views import *
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
-    url(r'^list$', RequestsListView.as_view(), name='list'),
     url(r'^create$', CreateRequestView.as_view(), name='create'),
+    url(r'^read$', ReadRequestView.as_view(), name='read'),
+    url(r'^update/(?P<pk>[0-9]+)/$', UpdateRequestView.as_view(), name='update'),
+    url(r'^delete/(?P<pk>[0-9]+)/$', DeleteRequestView.as_view(), name='delete'),
+    url(r'^api/', include('core.scrumboard.api.urls', namespace='api'))
 ]
