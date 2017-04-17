4fe0330dae5204cc50846023c37512032c1cb374
Kamilo Vasquez <me@killoblan.co>
2017-04-17 01:26:53 -0500

    setting up statics files urls

--- property_solutions/urls.py
+++ property_solutions/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
@@ -21,3 +23,6 @@ urlpatterns = [
     url(r'^', include('web.urls', namespace='web')),
     url(r'^admin/', include('core.urls', namespace='core'))
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
