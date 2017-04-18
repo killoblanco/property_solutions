e1148951899bff975e8b52c9c78d014b34ccb2e8
Kamilo Vasquez <me@killoblan.co>
2017-04-18 12:05:12 -0500

    add deadline chart data url

--- core/scrumboard/api/urls.py
+++ core/scrumboard/api/urls.py
@@ -4,4 +4,5 @@ from core.scrumboard.api.views import *
 urlpatterns = [
     url(r'type-chart-data$', RequestTypeChartData.as_view(), name='type_chart_data'),
     url(r'priority-chart-data$', RequestPriorityChartData.as_view(), name='priority_chart_data'),
+    url(r'deadline-chart-data$', RequestDeadlineChartData.as_view(), name='deadline_chart_data'),
 ]
